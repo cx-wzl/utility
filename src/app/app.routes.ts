f2568693501bd41072abc7c56959677ad06fa3d7
cx-wzl <luciferwzl@hotmail.com>
2025-06-24 17:35:30 +0800

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { FormBuilder } from './form-builder/form-builder';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'form/builder',
+  },
+  {
+    path: 'form/builder',
+    component: FormBuilder,
+  },
+];
---
+++ src/app/form-builder/form-builder.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'form-builder',
+  templateUrl: './form-builder.html',
+  styleUrls: ['./form-builder.scss'],
+})
+export class FormBuilder {}
